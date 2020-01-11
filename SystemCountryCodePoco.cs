dd598748b16ab486a46c0f80a82af1afa39f4bc3
sonymnidhiry <52722948+sonymnidhiry@users.noreply.github.com>
2020-01-11 15:03:49 -0500

    Initial Commit

---
+++ SystemCountryCodePoco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("System_Country_Codes")]
+    public class SystemCountryCodePoco
+    {
+        [Key]
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+    }
+
+}
